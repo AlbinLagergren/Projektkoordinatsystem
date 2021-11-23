cc1514816244f8c5711e00357179c5c408fd914c
AlbinLagergren <92784710+AlbinLagergren@users.noreply.github.com>
2021-11-23 12:41:47 +0100

    Add files via upload

---
+++ Projektarbete/Projektarbete/Pointscore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projektarbete
+{    
+    public class Pointscore
+    {
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int S { get; set; }
+
+
+
+    }
+}
+
+
---
+++ Projektarbete/Projektarbete/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projektarbete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // list.Add(new Circle(5, 3, 100));
+            List<Pointscore> coordinates = new List<Pointscore>()
+            {
+                new Pointscore() { X = -50, Y = 1 , S = 5 },
+                new Pointscore() { X = -50, Y = 1 , S = 5 },
+                new Pointscore() { X = -50, Y = 1 , S = 5 },
+                new Pointscore() { X = -50, Y = 1 , S = 5 },
+                new Pointscore() { X = -50, Y = 1 , S = 5 },
+                new Pointscore() { X = -50, Y = 1 , S = 5 }
+            };
+
+            List<Pointscore> shapes = new List<Pointscore>();
+            
+            coordinate square = new Square(5, 3, 100);
+            coordinate circle = new Circle(1, 3, 100);
+            
+        }   
+
+    }
+
+
+}
+
+
