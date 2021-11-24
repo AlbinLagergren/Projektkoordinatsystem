18bc4a7412a3568fed5b24e961ea3d721f672749
AlbinLagergren <92784710+AlbinLagergren@users.noreply.github.com>
2021-11-24 13:51:14 +0100

    Update Program.cs

--- Projektarbete/Projektarbete/Program.cs
+++ Projektarbete/Projektarbete/Program.cs
@@ -8,28 +8,46 @@ namespace Projektarbete
     {
         static void Main(string[] args)
         {
-            // list.Add(new Circle(5, 3, 100));
             List<Pointscore> listOfCoordinates = new List<Pointscore>()
-            {
+                {
                 new Pointscore() { X = -50, Y = 1 , S = 5 },
-                new Pointscore() { X = -50, Y = 1 , S = 5 },
-                new Pointscore() { X = -50, Y = 1 , S = 5 },
-                new Pointscore() { X = -50, Y = 1 , S = 5 },
-                new Pointscore() { X = -50, Y = 1 , S = 5 },
+                new Pointscore() { X = -10, Y = 5 , S = 5 },
                 new Pointscore() { X = -50, Y = 1 , S = 5 }
-            };
 
-            List<Pointscore> shapes = new List<Pointscore>();
-
-            coordinate square = new Square(5, 3, 100);
-            coordinate circle = new Circle(1, 3, 100);
+                };
 
             List<Shape> listOfShapes = new List<Shape>()
             { 
-                new Shape() {shapeName = "Circle", x = 2, y = 5, Perimeter = 10},
-                new Shape() {shapeName = "Square", x = 3, y = 8, Perimeter = 10}
+                    new Shape("Circle", 2, 2, 10), {shapeName = "Circle", x = 2, y = 5, Perimeter = 10}, 
+                    new Shape("Circle", 2, 2, 10) {shapeName = "Triangle", x = 2, y = 5, Perimeter = 10},
+                    new Shape() {shapeName = "Pentagon", x = 2, y = 5, Perimeter = 10},
+                    new Shape() {shapeName = "Hexagon", x = 2, y = 5, Perimeter = 10},
+                    new Shape() {shapeName = "Heptagon", x = 2, y = 5, Perimeter = 10},
+                    new Shape() {shapeName = "Octagon", x = 2, y = 5, Perimeter = 10},
+                    new Shape() {shapeName = "Square", x = 3, y = 8, Perimeter = 10}
+
+            };
+            
+            
+
+            List<ShapeScore> listOfShapesScore = new List<ShapeScore>()
+            {
+
+                new ShapeScore() { shapeName = "Circle", shapePoints = 100 },
+                new ShapeScore() { shapeName = "Circle", shapePoints = 100 },
+                new ShapeScore() { shapeName = "Circle", shapePoints = 100 },
+                new ShapeScore() { shapeName = "Circle", shapePoints = 100 },
+                new ShapeScore() { shapeName = "Circle", shapePoints = 100 },
+                new ShapeScore() { shapeName = "Circle", shapePoints = 100 },
+                new ShapeScore() { shapeName = "Circle", shapePoints = 100 }
             };
 
+        }
+
+        List<Pointscore> shapes = new List<Pointscore>();
+
+        coordinate square = new Square(5, 3, 100);
+        coordinate circle = new Circle(1, 3, 100);
 
         }
     }
