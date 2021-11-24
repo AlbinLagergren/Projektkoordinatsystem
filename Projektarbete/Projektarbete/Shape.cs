4409239f1c3249737b76a602183bba426353f1de
AlbinLagergren <92784710+AlbinLagergren@users.noreply.github.com>
2021-11-24 13:51:55 +0100

    Update Shape.cs

--- Projektarbete/Projektarbete/Shape.cs
+++ Projektarbete/Projektarbete/Shape.cs
@@ -6,14 +6,22 @@ namespace Projekt
 {
     class Shape
     {
-        public string shapeName = "";
-        public int x;
-        public int y;
-        public int Perimeter;
+        
+        public int x { get; set; }
+        public int y { get; set; }
+        public int Perimeter { get; set; }
+        public string shapeName { get; set; }
 
+        public Shape (string shapeName, int x, int y, int Perimeter)
+        {
+            this.shapeName = shapeName;
+            this.x = x;
+            this.y = y;
+            this.Perimeter = Perimeter;
+        }
         class Circle : Shape
         {
-            public Circle (string shapeName, int x, int y, int Perimeter)
+            public Circle(string shapeName, int x, int y, int Perimeter) : base (shapeName, x, y, Perimeter)
             {
                 this.shapeName = shapeName;
                 this.x = x;
@@ -24,7 +32,17 @@ namespace Projekt
         }
         class Square : Shape
         {
-            public Square (string shapeName, int x, int y, int Perimeter)
+            public Square(string shapeName, int x, int y, int Perimeter) : base(shapeName, x, y, Perimeter)
+            {
+                this.shapeName = shapeName;
+                this.x = x;
+                this.y = y;
+                this.Perimeter = Perimeter;
+            }
+        }
+        class Triangle : Shape
+        {            
+            public Triangle(string shapeName, int x, int y, int Perimeter) : base(shapeName, x, y, Perimeter)
             {
                 this.shapeName = shapeName;
                 this.x = x;
@@ -33,12 +51,55 @@ namespace Projekt
             }
         }
 
-        static bool inShape(string shapeName, int x, int y)
+        class Pentagon : Shape
         {
-            if(shapeName == "Circle")
+            public Pentagon(string shapeName, int x, int y, int Perimeter) : base(shapeName, x, y, Perimeter)
             {
-                
+                this.shapeName = shapeName;
+                this.x = x;
+                this.y = y;
+                this.Perimeter = Perimeter;
             }
         }
+        
+        class Hexagon : Shape
+        {
+            public Hexagon(string shapeName, int x, int y, int Perimeter) : base(shapeName, x, y, Perimeter)
+            {
+                this.shapeName = shapeName;
+                this.x = x;
+                this.y = y;
+                this.Perimeter = Perimeter;
+            }
+        }
+        class Heptagon : Shape
+        {
+            public Heptagon(string shapeName, int x, int y, int Perimeter) : base(shapeName, x, y, Perimeter)
+            {
+                this.shapeName = shapeName;
+                this.x = x;
+                this.y = y;
+                this.Perimeter = Perimeter;
+            }
+
+        }
+        class Octagon : Shape
+        {
+            public Octagon(string shapeName, int x, int y, int Perimeter) : base(shapeName, x, y, Perimeter)
+            {
+                this.shapeName = shapeName;
+                this.x = x;
+                this.y = y;
+                this.Perimeter = Perimeter;
+            }
+        }
+
+        /*static bool inShape(string shapeName, int x, int y)
+        {
+            if (shapeName == "Circle")
+            {
+
+            }
+        }*/
     }
 }
