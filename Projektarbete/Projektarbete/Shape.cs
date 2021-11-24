e51fb3b80e396d981cca5d3a57d21fd100c6326a
AlbinLagergren <92784710+AlbinLagergren@users.noreply.github.com>
2021-11-24 09:10:17 +0100

    Add files via upload

---
+++ Projektarbete/Projektarbete/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projekt
+{
+    class Shape
+    {
+        public string shapeName = "";
+        public int x;
+        public int y;
+        public int Perimeter;
+
+        class Circle : Shape
+        {
+            public Circle (string shapeName, int x, int y, int Perimeter)
+            {
+                this.shapeName = shapeName;
+                this.x = x;
+                this.y = y;
+                this.Perimeter = Perimeter;
+            }
+
+        }
+        class Square : Shape
+        {
+            public Square (string shapeName, int x, int y, int Perimeter)
+            {
+                this.shapeName = shapeName;
+                this.x = x;
+                this.y = y;
+                this.Perimeter = Perimeter;
+            }
+        }
+
+        static bool inShape(string shapeName, int x, int y)
+        {
+            if(shapeName == "Circle")
+            {
+                
+            }
+        }
+    }
+}
