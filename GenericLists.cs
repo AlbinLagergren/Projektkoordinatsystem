7701c66ca30cb0d6c81c1242fe13b241cd0595b8
Bellmaestro <94905491+Bellmaestro@users.noreply.github.com>
2021-11-26 11:55:14 +0100

    Add files via upload

---
+++ GenericLists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace OOP1
+{
+    public class GenericLists
+    {
+        public GenericLists()
+        {
+            
+
+                List<Coordinates> listOfCoordinates = new List<Coordinates>();
+
+                List<Shape> listOfShapes = new List<Shape>();
+
+            listOfShapes.Add(new Shape("Circle", 2, 2, 10));
+
+            Dictionary<string, int> listOfShapeScore = new Dictionary<string, int>();
+            listOfShapeScore.Add("Circle", 15);
+
+                
+
+                
+
+        }
+
+        
+
+    }
+}
